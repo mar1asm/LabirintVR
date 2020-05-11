e7e84c7ac5234d00e2c52a69a0c02198619349b9
Maria <52409116+mar1asm@users.noreply.github.com>
2020-05-11 22:24:06 +0300

    Add files via upload

---
+++ PathNode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode {
+    public int xValue;
+    public int zValue;
+    public int gCost;
+    public int hCost;
+    public int fCost;
+    public PathNode previousNode;
+
+    public void CalculateCost()
+    {
+        fCost = gCost + hCost;
+    }
+}
